eb28c53355c9ecc4d555c6e5478b02a3af8da858
antonhatswag <anton.foerster.bremen@gmail.com>
2021-10-31 22:34:03 +0100

    garden app v1

--- src/main/java/com/minf1_git/minf_ue1_git/View.java
+++ src/main/java/com/minf1_git/minf_ue1_git/View.java
@@ -11,8 +11,8 @@ public class View extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(View.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Awesome Garden v2.0");
         stage.setScene(scene);
         stage.show();
     }
