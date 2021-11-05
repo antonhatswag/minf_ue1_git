defcd42ce4b450fddfbc269e7dc86451129f7280
anton <y.fr34k.lp@gmail.com>
2021-11-05 12:07:13 +0100

    file name changes

--- src/main/java/com/minf1_git/minf_ue1_git/View.java
+++ src/main/java/com/minf1_git/minf_ue1_git/View.java
@@ -3,6 +3,7 @@ package com.minf1_git.minf_ue1_git;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +11,16 @@ import java.io.IOException;
 public class View extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(View.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        stage.setTitle("Awesome Garden v2.0");
+        stage.setTitle("Awesome Garden v1.0");
+        stage.getIcons().add(
+                new Image(
+                        View.class.getResourceAsStream( "sunflower.png" )));
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
